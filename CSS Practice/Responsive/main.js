64bc0688210d3b69033b479a28252afe11f7f573
Ian <ian.manalo1026@gmail.com>
2022-02-14 11:42:13 +0800

    loading

--- CSS Practice/Responsive/main.js
+++ CSS Practice/Responsive/main.js
@@ -21,3 +21,13 @@ modaClose.addEventListener("click", function () {
 contactBtn.addEventListener("click", function () {
   contactDetails.classList.add("contact-active");
 });
+
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
